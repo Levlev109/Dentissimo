01f18b0ce05dcc0c60a393e221a4b509e30162be
henwl <henwl@github.com>
2026-03-05 19:31:33 +0100

    Compress video 16.5MB->4.1MB (720p CRF28), enable preload

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = () => {
           muted
           loop
           playsInline
-          preload="none"
+          preload="auto"
           className="hidden md:block w-full h-full object-cover"
           poster="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'%3E%3Crect fill='%231c1917' width='1' height='1'/%3E%3C/svg%3E"
         >
